5f9a78181aac6fb1a0390ce3d8b696eaf66e3a12
Sigurd Gaukstad <sigurgau@sigurds-mbp.lan>
2023-12-26 10:04:30 +0100

    ok

--- mlp_xyz.py
+++ mlp_xyz.py
@@ -3,29 +3,16 @@ from torch import nn
 from torch.nn import functional as F
 
 class MLP(torch.nn.Module):
-    def __init__(self,input_dim):
+    def __init__(self,dims = [64,28,3]):
         super(MLP,self).__init__()
-        self.model = nn.Sequential(
-            #nn.Flatten(),
-            nn.Dropout(0.1),
-            nn.Linear(input_dim, 264),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(264, 128),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(128, 64),
-            nn.ReLU(),
-            nn.Dropout(0.1),
-            nn.Linear(64, 3),
-            #nn.ReLU(),
-            #nn.Dropout(0.1),
-            #nn.Linear(32, 16),
-            #nn.ReLU(),
-            #nn.Dropout(0.1),
-            #nn.Linear(16, 3),
+        self.layers = []
+        for i in range(len(dims)-1):
+            self.layers.append(nn.Dropout(0.1))
+            self.layers.append(nn.Linear(dims[i], dims[i+1]))
+            self.layers.append(nn.ReLU())
+            
+        self.model = nn.Sequential(*self.layers
         )
-        
         self.double()
 
     def forward(self, x):
