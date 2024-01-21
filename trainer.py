fe5171215e6962e9eff141c157a8ecd6dbbf3f98
Sigurd Gaukstad <sigurdgau@pm.me>
2024-01-21 17:12:18 +0100

    pretrain

--- trainer.py
+++ trainer.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 import torch
 import tqdm
 
-def train(model, train_loader,val_loader , optimizer, criterion, epochs, device = "cpu"):
+def train(model, train_loader,val_loader , optimizer, criterion, epochs, device = "cpu", pretrain = ""):
     model.train()
     model.to(device)
     
@@ -13,30 +13,48 @@ def train(model, train_loader,val_loader , optimizer, criterion, epochs, device 
         for epoch in lossepoch:
             l = 0
             for data in train_loader:  # Iterate in batches over the training dataset.
-                 out = model(data[0].to(device))
-                 loss_fl = criterion(out, data[0].to(device))
+                 if pretrain == "encoder":
+                     out = model.encoder(data[0].to(device))
+                     out = torch.sin(out*torch.pi) 
+                     loss_fl = criterion(out, data[1].to(device))
+                 elif pretrain == "decoder":
+                     out = model.decoder(data[1]).to(device)
+                     loss_fl = criterion(out, data[0].to(device))
+                 else:
+                     out = model(data[0].to(device))
+                     loss_fl = criterion(out, data[0].to(device))
+    
+                
                  loss_fl.backward()  # Derive gradients.
                  l += loss_fl.cpu().detach().numpy()
                  optimizer.step()  # Update parameters based on gradients.
                  optimizer.zero_grad()  # Clear gradients.
             l = l/len(train_loader)
             train_loss.append(l)
-            v = val(model, val_loader, optimizer, criterion, device)
+            v = val(model, val_loader, optimizer, criterion, device, pretrain)
             val_loss.append(v)
             lossepoch.set_description(f"Train loss = {l}, Val loss = {v}")
     
     return train_loss, val_loss
 
-def val(model, loader, optimizer, criterion, device):
+def val(model, loader, optimizer, criterion, device, pretrain):
      model.eval()
      model.to(device)
      l = 0
      for data in loader:  
-        
-         out = model(data[0].to(device)) 
-         loss_fl = criterion(out, data[0].to(device))
+         if pretrain == "encoder":
+            out = model.encoder(data[0].to(device))
+            out = torch.sin(out*torch.pi) 
+            loss_fl = criterion(out, data[1].to(device))
+         elif pretrain == "decoder":
+            #out = model.decoder(torch.sin(data[1]*torch.pi).to(device))
+            out = model.decoder(data[1]).to(device)
+            loss_fl = criterion(out, data[0].to(device))
+         else:
+            out = model(data[0].to(device))
+            loss_fl = criterion(out, data[0].to(device))
+         
          l += loss_fl.cpu().detach().numpy()
     
      return l/len(loader)
-
 
